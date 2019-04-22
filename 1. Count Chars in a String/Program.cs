6a2d3d417cb00588397809f1b5d106a63582c7f9
don-kokone <don_kokone@start.bg>
2019-04-22 21:55:19 +0300

    Importing project

---
+++ 1. Count Chars in a String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string arr = Console.ReadLine();
+            //char[] nums = arr.ToCharArray();
+            char[] nums = Console.ReadLine().Where(x => x != ' ').ToArray();
+
+
+            var counts = new Dictionary<char, int>();
+
+            foreach (var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
---
+++ 2. A Miner Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resoursec = new Dictionary<string, int>();
+
+            string resource = string.Empty;
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resoursec.ContainsKey(resource))
+                {
+                    resoursec[resource] = quantity;
+                }
+                else
+                {
+                    resoursec[resource] = resoursec[resource] + quantity;
+                }
+            }
+
+            foreach (var kvp in resoursec)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+        }
+    }
+}
